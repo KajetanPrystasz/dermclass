da0795a892060c2a37ac13e3e20f7580546f622d
KajetanPrystasz <kajetanprystasz@gmail.com>
2020-11-01 12:50:12 +0100

    Fix pickling

--- src/dermclass_models/dermclass_models/base/pickle.py
+++ src/dermclass_models/dermclass_models/base/pickle.py
@@ -13,7 +13,6 @@ class Pickle:
     def __init__(self, config: BaseConfig):
         self.config = config
 
-        self.pipeline_type = self.config.pipeline_type
         self.pipeline_version = _version
 
         self.logger = logging.getLogger(__name__)
@@ -22,7 +21,7 @@ class Pickle:
     def load_pipeline(self, path: str = None):
         """Load pipeline from to pickle folder using file name"""
         if path is None:
-            path = self.config.PICKLE_DIR / f"{self.pipeline_type}_{self.pipeline_version}.pkl"
+            path = self.config.PICKLE_DIR / f"{self.config.PIPELINE_TYPE}_{self.pipeline_version}.pkl"
 
         file_name = path.name
         trained_model = joblib.load(filename=path)
@@ -34,7 +33,7 @@ class Pickle:
     def save_pipeline(self, pipeline_object: Pipeline):
         """Save pipeline to pickle folder"""
 
-        save_file_name = f"{self.pipeline_type}_{self.pipeline_version}.pkl"
+        save_file_name = f"{self.config.PIPELINE_TYPE}_{self.pipeline_version}.pkl"
         save_path = self.config.PICKLE_DIR / save_file_name
 
         self.remove_old_pipelines(pipelines_to_keep=[save_file_name])
@@ -45,9 +44,9 @@ class Pickle:
     def remove_old_pipelines(self, pipelines_to_keep: List[str]):
         """Remove pickles from pickle folder except the files in files_to_keep variable"""
 
-        do_not_delete = pipelines_to_keep + ['__init__.py']
+        do_not_delete = pipelines_to_keep + ['__init__.py', ".gitkeep"]
 
         for file in self.config.PICKLE_DIR.iterdir():
-            if file.name not in do_not_delete and file.name.startswith(self.PIPELINE_TYPE):
+            if file.name not in do_not_delete and file.name.startswith(self.config.PIPELINE_TYPE):
                 file.unlink()
                 self.logger.info(f"{file} removed")
