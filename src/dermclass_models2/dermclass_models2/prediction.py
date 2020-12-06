eeb15172a6ce2e84196c3c394ea5df7a3028d38c
KajetanPrystasz <kajetanprystasz@gmail.com>
2020-12-06 19:29:41 +0100

    Add dir

--- src/dermclass_models2/dermclass_models2/prediction.py
+++ src/dermclass_models2/dermclass_models2/prediction.py
@@ -10,14 +10,7 @@ from dermclass_models2.base.pickle import Pickle
 from dermclass_models2.base.processing.validation import Validation
 from dermclass_models2 import __version__ as _version
 
-
-class Validator:
-    def validate_input(*args, **kwargs):
-        return args, kwargs
-
-    # df_valid = self.validator.validate_columns(df)
-    # df_reordered = self.validator.reorder_df(df_valid)
-    # df_final = self.validator.custom_validation(df_reordered)
+# TODO: Change gitingore for pickles and datasets
 
 
 class _Prediction(abc.ABC):
@@ -56,49 +49,3 @@ class _Prediction(abc.ABC):
                          f"Inputs: {data} "
                          f"Predictions: {prediction}")
         return prediction
-
-
-class _SklearnPrediction(abc.ABC, _Prediction):
-    def _prepare_data_to_right_format_sklearn(self):
-        pass
-
-    def _make_prediction_sklearn(self):
-        pass
-
-
-class _TFPrediction(abc.ABC, _Prediction):
-    def _prepare_data_to_right_format_tf(self):
-        pass
-
-    def _make_prediction_tf(self):
-        pass
-
-
-class StructuredPrediction(_SklearnPrediction):
-    def _prepare_data_to_right_format(self):
-        self._prepare_data_to_right_format_sklearn()
-
-    def _make_prediction(self):
-        self._prepare_data_to_right_format_sklearn()
-
-
-class TextPrediction(_SklearnPrediction, _TFPrediction):
-    def _prepare_data_to_right_format(self, formatt="sklearn"):
-        if formatt == "sklearn":
-            self._prepare_data_to_right_format_sklearn()
-        else:
-            self._prepare_data_to_right_format_tf()
-
-    def _make_prediction(self, formatt="sklearn"):
-        if formatt == "sklearn":
-            self._make_prediction_sklearn()
-        else:
-            self._make_prediction_tf()
-
-
-class ImagePrediction(_TFPrediction):
-    def _prepare_data_to_right_format(self):
-        self._prepare_data_to_right_format_tf()
-
-    def _make_prediction(self):
-        self._prepare_data_to_right_format_tf()
